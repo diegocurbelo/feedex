65435425caced9e2f73305d328796edc1f6681e6
Diego Curbelo <diego@curbelo.com>
2017-08-31 10:14:01 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Feedex.Mixfile do
 
   def project do
     [app: :feedex,
-     version: "0.0.2",
+     version: "0.1.0",
      elixir: "~> 1.4",
      description: "Elixir Feed Parser",
      package: package(),
